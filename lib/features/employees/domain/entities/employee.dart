8552712ec4008e82bcbdff41a414930f04b2e579
Oleksandr <sanyagolikov97@gmail.com>
2023-10-26 05:35:59 +0300

    Employee model

---
+++ lib/features/employees/domain/entities/employee.dart
@@ -0,0 +1,25 @@
+class Employee {
+  final int id;
+  final String name;
+  final String surname;
+  final String salary;
+  final String department;
+  final String position;
+  final String adress;
+  final DateTime? birthDate;
+  final DateTime? hireDate;
+  final DateTime? leavhDate;
+
+  Employee({
+    required this.id,
+    required this.name,
+    required this.surname,
+    required this.salary,
+    required this.department,
+    required this.position,
+    required this.adress,
+    this.birthDate,
+    this.hireDate,
+    this.leavhDate,
+  });
+}
